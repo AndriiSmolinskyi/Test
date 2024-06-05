ccc2f20fa3780839099e20cdab3b83be6121b9ac
AndriiSmolinskyi <awful.braid@gmail.com>
2024-06-05 15:52:23 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,98 @@
+// alert('hello')
+// console.log('Hello world')
+
+
+//змінні
+
+let a  = 'Hy world'
+
+// dobrogovechoramyzukrainy
+
+let pesPatron
+
+const catAmmo = 100
+
+let age
+// age = prompt("Введіть свій вік")
+
+// alert(age)
+
+
+// let name = 'andrii'
+// let surname = 'smolinksyi'
+// console.log(name, surname)
+
+
+//створити 2 змінних президент1 президент2
+//за допомогою промта записати в змінні імена президентів
+//і потім за допомогою алертів це вивести
+
+
+// Оголосіть дві змінні: admin та name.
+// Присвойте значення "Іван" змінній name.
+// Скопіюйте значення зі змінної name в admin.
+// Виведіть значення змінної admin, використовуючи функцію alert (яка повинна показати “Іван”).
+
+let name1 = 'Іван'
+let admin = name1
+console.log(name1)
+
+//Створіть програму, яка обчислює суму всіх простих чисел в заданому діапазоні.
+
+
+let count = 1
+
+// let name2 = prompt("Введіть своє імя")
+// alert(`Вітаємо вас ${name2}`) 
+
+let isBigger = 1 < 4
+console.log(isBigger)
+
+let zero = null
+console.log(zero)
+
+let salary = 5000
+
+salary = undefined
+console.log(salary)
+
+salary = 5000
+
+console.log(typeof zero)
+
+
+let x;
+
+x = 1  //number
+x = 1241242135213521354353245423234542353245  //bigint
+x = 'Hello'  //string
+x = true     //boolean
+x = null     //null
+x = undefined //undefined 
+
+x = {
+    name: 'Kiril',
+    surname: 'Shevchenko'
+}
+
+typeof x  
+
+// let isBoss = confirm("Ви бос?");   
+
+console.log('///////////////////////////////////')
+
+let value = true
+console.log(typeof value)
+value = String(value)
+console.log(typeof value)
+
+value = "1"
+let sum = "312321"
+console.log(Boolean(sum))
+
+
+let y = 5; 
+let z = 2;
+y--
+let result = y
+console.log(result)
